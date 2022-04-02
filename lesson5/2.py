96dc60b377626d1460f5af4c550ad48837051e62
apl <apl85@list.ru>
2022-04-02 17:18:15 +0600

    lesson5

---
+++ lesson5/2.py
@@ -0,0 +1,12 @@
+num = 7
+num_gen = (i for i in range(1, num + 1, 2))
+
+print(next(num_gen))
+print(next(num_gen))
+print(next(num_gen))
+print(next(num_gen))
+print(next(num_gen))
+print(next(num_gen))
+print(next(num_gen))
+print(next(num_gen))
+
